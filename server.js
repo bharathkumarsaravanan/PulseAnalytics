871c0ee81ee602ff6ff0c2668c7a6fdea2758b7a
bharathkumarsaravanan <bharathsaravananofficial@gmail.com>
2026-05-14 22:02:19 +0530

    server fixed

--- server.js
+++ server.js
@@ -50,9 +50,9 @@ nextApp.prepare().then(() => {
     return handle(req, res);
   });
 
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on port:${PORT}`);
   });
 });
